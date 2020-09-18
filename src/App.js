e80f7e8e745ba7986d5a9fade7081bac3b61adb0
jeff <jeffangotti@gmail.com>
2020-09-18 22:01:28 +0000

    working on basics in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App	extends Component {
+	foo2 = ()=>'Bars'
+  render(){
+		const name = 'John Doe';
+		const foo = ()=>'Bar';
+		const loading = false;
+		const showName= true;
+	
+		return (
+			//JSX is the html code below java script synatx extention XML or html in a way
+			// under the hood its actually javascript ,,, JSX needs one parent element such as the div 
+			// we can use fragments instead of div or empty brackets <>
+			//use classname instead of class
+			//<React.Fragment>
+			//	<h2>hello</h2>
+			//</React.Fragment>
+			// or no jsx 	return React.createElement('div',{className:'App'}, React.createElement('h1',null,'hello from react'));
+    	
+			
+			
+			<div className="App">
+				{loading ? <h4>loading</h4> : <h2>not loading</h2>}
+				<h1>Hello {showName && name}</h1>
+				<h1>Hello {1+1}</h1>
+				<h1>Hello {foo()}</h1>
+				<h1>Hello {this.foo2()}</h1>
+    	</div>
+  	);
+	}
 }
 
 export default App;
