4ed27232a0a3a8d6d93b4f788e5a8a993d026f9e
jeff <jeffangotti@gmail.com>
2020-09-20 19:33:52 +0000

    refactor Navbar and UserItem from class components to funciton components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,28 @@
 //classbased componet 
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
+const Navbar = ({icon,title}) =>{
+ 
+	return (
+		<nav className="navbar bg-primary">
+			<h1>
+				<i className={icon}/> {title}
+			</h1>
+		</nav>
+	);
+}
+
+
 	//default props for components
-	static defaultProps = {
+	Navbar.defaultProps = {
 		title:'Github Finder',
 		icon:'fa fa-github'
 	};
 	
-	static propTypes = {
+	Navbar.propTypes = {
 		title: PropTypes.string.isRequired,
 		icon:  PropTypes.string.isRequired
 	};
-
-  render() {
-    return (
-			<nav className="navbar bg-primary">
-				<h1>
-					<i className={this.props.icon}></i> {this.props.title}
-				</h1>
-			</nav>
-			);
-  }
-}
 
 export default Navbar
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class UserItem extends Component {
-	render() {
-		const {login, avatar_url,html_url} = this.props.user;
-		
-    return <div className='card text-center'>
-		<img 
-			src={avatar_url} 
-			alt=''
-			className='round-img' 
-			style={{width:'60px'}} 
+const UserItem = ({user: {login, avatar_url,html_url}}) => {
+	return (
+		<div className='card text-center'>
+			<img 
+				src={avatar_url} 
+				alt=''
+				className='round-img' 
+				style={{width:'60px'}} 
 			/>
 			<h3>{login}</h3>
 			<div>
 				<a href={html_url} className='btn btn-dark btn-sm my-1'>More</a>
 			</div>
-		</div>;
-  }
-}
+		</div>
+	);
+};
+
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired,
+};
 
 export default UserItem
