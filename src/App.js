a6b1920c0bab61c2cbd1782ef170debd0722d85d
jeff <jeffangotti@gmail.com>
2020-09-28 17:31:11 +0000

    add alert context API provider

--- src/App.js
+++ src/App.js
@@ -1,93 +1,38 @@
-import React, {Fragment, useState} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';//GithubSearch/github_finder/src/components/layout/Navbar.js
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
-import axios from 'axios'// api fetching package used to 
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
 
 import './App.css';
 
 
 const App	= () =>{
-	const [repos, setRepos] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [alert, setAlert] = useState(null);
-
-	//search github users 
 	
-
-	//get a single github user
-
-	//get users repos
-	const getUserRepos = async (username) =>{
-		setLoading(true);
-		
-		const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
-			{process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-			{process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-		
-		setRepos(res.data);
-		setLoading(false);
-	};
-
-	//clear users from state 
-
-
-	//set alert
-	const showAlert = (msg, type) => {
-		setAlert({msg, type});
-		//make it go away after a certain time 
-		setTimeout(()=> setAlert(null),5000)
-	};
-		
 	return (
 		<GithubState>
+		<AlertState>
 		<Router>
 			<div className="App">
 				<Navbar />
 				<div className='container'>
-					<Alert
-						alert={alert}
-					/>
+					<Alert/>
 					<Switch>
-						<Route 
-							exact 
-							path='/' 
-							render={props => (
-								<Fragment>
-									<Search  
-										setAlert={showAlert}
-									/>
-									<Users />	
-								</Fragment>
-							)} 
-						/>
-						<Route
-							exact
-							path="/about"
-							component={About}
-						/>
-						<Route 
-							exact 
-							path='/user/:login' 
-							render={props => (
-								<Fragment>
-									<User 
-										{ ...props }
-										getUserRepos={getUserRepos}
-										repos={repos}
-									/>	
-								</Fragment>
-						)} />
+						<Route exact path='/' component={Home} />
+						<Route exact path="/about" component={About} />
+						<Route exact path='/user/:login' component = {User} />
+						<Route component = {NotFound} />
 					</Switch>
 				</div>
 			</div>
 		</Router>
+		</AlertState>
 		</GithubState>
 	);
 }
