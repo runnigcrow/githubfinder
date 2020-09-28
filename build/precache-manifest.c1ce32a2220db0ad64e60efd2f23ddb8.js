d4218c36d95f5505a1f0ca49fe78c9b7a8556c6a
jeff <jeffangotti@gmail.com>
2020-09-28 17:52:34 +0000

    create a build directory

---
+++ build/precache-manifest.c1ce32a2220db0ad64e60efd2f23ddb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accf27451316290e634e6358dc5f5a86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db2c66bc45a0cbc20341",
+    "url": "/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "0f6057c39770833bfdc7",
+    "url": "/static/js/2.a848e06f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a848e06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2c66bc45a0cbc20341",
+    "url": "/static/js/main.fe9b23bf.chunk.js"
+  },
+  {
+    "revision": "4b4a6179411862e635e1",
+    "url": "/static/js/runtime-main.159ac571.js"
+  }
+]);
