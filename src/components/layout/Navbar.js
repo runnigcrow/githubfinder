d89f571ba66b3e41424f2edb5f58a3b04f9c7576
Jeff Angotti <jeffangotti@gmail.com>
2020-12-05 22:47:52 -0500

    revison

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,37 +1,35 @@
-//classbased componet 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom'
+//classbased componet
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const Navbar = ({icon,title}) =>{
- 
-	return (
-		<nav className="navbar bg-primary">
-			<h1>
-				<i className={icon}/> {title}
-			</h1>
-			<ul>
-				<li>
-					<Link to='/'>Home</Link>
-				</li>
-				<li>
-					<Link to='/about'>About</Link>
-				</li>
-			</ul>
-		</nav>
-	);
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
+//default props for components
+Navbar.defaultProps = {
+  title: "Github Finder 2",
+  icon: "fa fa-github",
+};
 
-	//default props for components
-	Navbar.defaultProps = {
-		title:'Github Finder',
-		icon:'fa fa-github'
-	};
-	
-	Navbar.propTypes = {
-		title: PropTypes.string.isRequired,
-		icon:  PropTypes.string.isRequired
-	};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
-export default Navbar
+export default Navbar;
